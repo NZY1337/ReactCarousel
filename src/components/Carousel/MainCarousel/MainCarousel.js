c3eaebcd8a9dc332f0b09940361f72bb03e1fd6b
NZY1337 <mandreicosmin@yahoo.com>
2020-06-12 16:50:07 +0300

    dots animation added - finished

--- src/components/Carousel/MainCarousel/MainCarousel.js
+++ src/components/Carousel/MainCarousel/MainCarousel.js
@@ -53,7 +53,16 @@ class Carousel extends Component {
             },
 
             {
-                id: 2,
+                id: 4,
+                url:
+                "https://images.pexels.com/photos/106936/pexels-photo-106936.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                name: "Look At Me Now",
+                shown: false,
+                category: 'abstract'
+            },
+
+            {
+                id: 5,
                 url:
                 "https://images.pexels.com/photos/271795/pexels-photo-271795.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
                 name: "A Day To Remember",
@@ -62,23 +71,26 @@ class Carousel extends Component {
             },
 
             {
-                id: 3,
+                id: 6,
                 url:
-                "https://images.pexels.com/photos/106936/pexels-photo-106936.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-                name: "Look At Me Now",
+                "https://images.pexels.com/photos/584399/living-room-couch-interior-room-584399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                name: "In July",
                 shown: false,
-                category: 'abstract'
-            }
+                category: 'outdoor'
+            },
+
+            {
+                id: 7,
+                url:
+                "https://images.pexels.com/photos/271795/pexels-photo-271795.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                name: "A Day To Remember",
+                shown: false,
+                category: 'minimalistic'
+            },
         ]
     };
    
-    componentDidMount() {
-        this.setSpacer();
-    }
-
-    componentDidUpdate() {
-        this.setSpacer();
-    }
+    
     
     getFirstLetter(item) {
         return item.name.split('').splice(0,1)
@@ -116,13 +128,34 @@ class Carousel extends Component {
         })
     };    
     
+    componentDidMount() {
+        this.setSpacer();
+    }
+
+    componentDidUpdate() {
+        this.setSpacer();
+    }
+
     setSpacer() {
-        const hr = document.querySelector('#spacer');
-        const dotsWrapper = document.querySelector('.dots-wrapper');
-        const dotsWrapperWidth = ((this.state.cards.length-this.state.slideIndex) * 12) + (20 * (this.state.cards.length - this.state.slideIndex)) -20;
-        const asd = document.querySelector('#asd');
+        const spacer = document.querySelector('#spacer');
+        let dotsWrapperWidth;
+
+        if (this.state.slideIndex >= Math.round(this.state.cards.length / 2)) {
+            dotsWrapperWidth = ((this.state.cards.length-(this.state.cards.length - this.state.slideIndex - 1)) * 12) + (20 * (this.state.cards.length - (this.state.cards.length-this.state.slideIndex - 1))) -20;
+            spacer.classList.add('spacer-style2');
+            spacer.classList.remove('spacer-style1');
+
+        } else {
+            // gap = 20; box width = 12;
+            // [(cards.length - current index) * 12] + [(cards.length - current index)] - 1 gap(20) -> last box doesn't have a gap;
+            // (boxes width + (boxes gap - 1);
             
-        hr.style.width =  `${dotsWrapperWidth - 6}px`;
+            dotsWrapperWidth = ((this.state.cards.length-this.state.slideIndex) * 12) + (20 * (this.state.cards.length - this.state.slideIndex) -20);
+            spacer.classList.add('spacer-style1');
+            spacer.classList.remove('spacer-style2');
+        }
+        
+        spacer.style.width =  `${dotsWrapperWidth - 6}px`;
     }   
 
     render() {
@@ -187,8 +220,6 @@ class Carousel extends Component {
             </div>
         );
 
-       
-        
         return (
             <div className="carousel-wrapper position-relative">
                 <div className="container-fluid h-100 React-Carousel" style={bgImg}>
