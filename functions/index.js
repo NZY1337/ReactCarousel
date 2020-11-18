a1f06d0a23b96c093e7ab09cce61fdbc80546230
NZY1337 <mandreicosmin@yahoo.com>
2020-11-18 20:37:57 +0200

    changind the way i send emails to clients

--- functions/index.js
+++ functions/index.js
@@ -17,11 +17,21 @@ var transport = nodemailer.createTransport({
     }
 });
 
-exports.contactForm = functions.firestore.document('welcome/{id}').onCreate((snap, context) => {
-    const email = snap.data().email;
-    const name = snap.data().name;
-    const message = snap.data().message;
-    return sendContactForm(email, name, message);
+exports.contactForm = functions.https.onRequest((req, res) => {
+    const email = req.body.email;
+    const name = req.body.name;
+    const message = req.body.message;
+
+    cors(req, res, () => {
+        if (req.method !== 'POST') {
+            return;
+        }
+
+        // eslint-disable-next-line consistent-return
+        return sendContactForm(email, name, message);
+    })
+
+
 })
 
 async function sendContactForm(email, name, message) {
@@ -41,3 +51,27 @@ async function sendContactForm(email, name, message) {
         return e;
     }
 }
+
+// exports.submit = functions.https.onRequest((req, res) => {
+
+//     cors(req, res, () => {
+//         if (req.method !== 'POST') {
+//             return;
+//         }
+
+//         const mailOptions = {
+//             from: req.body.email,
+//             replyTo: req.body.email,
+//             to: gmailEmail,
+//             subject: `from my website ${req.body.email}`,
+//             text: req.body.message,
+//             html: `<p>${req.body.message}`
+//         };
+
+//         mailTransport.sendMail(mailOptions);
+
+//         res.status(200).end();
+//         // or you can pass data to indicate success.
+//         res.status(200).send({ isEmailSend: true });
+//     });
+// });
