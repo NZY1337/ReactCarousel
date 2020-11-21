12c7fbbe2b26a97c4d42f99528952b84afcdb562
Andrei <andrei.mocanu@cybertech.ro>
2020-11-22 01:16:56 +0200

    cloud function is now working with a gmail account

--- functions/index.js
+++ functions/index.js
@@ -5,55 +5,61 @@ const nodemailer = require("nodemailer");
 const cors = require("cors")({ origin: true });
 admin.initializeApp();
 
-const user = "a7eb9ce78fb6db";
-const pass = "a49db7431f7575";
+// const user = "a7eb9ce78fb6db";
+// const pass = "a49db7431f7575";
 
+// var transport = nodemailer.createTransport({
+// 	host: "smtp.mailtrap.io",
+// 	port: 2525,
+// 	auth: {
+// 		user: user,
+// 		pass: pass,
+// 	},
+// });
 
-var transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-        user: user,
-        pass: pass
-    }
+const user = "mandreicosmin1990@gmail.com";
+const pass = "ofmjnyjesndfdfyv";
+
+let transport = nodemailer.createTransport({
+	service: "Gmail",
+	auth: {
+		user: user,
+		pass: pass,
+	},
 });
 
 exports.contactForm = functions.https.onRequest((req, res) => {
+	cors(req, res, () => {
+		if (req.method !== "POST") {
+			return;
+		}
 
-    cors(req, res, () => {
-        if (req.method !== 'POST') {
-            return;
-        }
+		const email = req.body.email;
+		const name = req.body.name;
+		const message = req.body.message;
 
-        const email = req.body.email;
-        const name = req.body.name;
-        const message = req.body.message;
+		sendContactForm(email, name, message);
 
-        sendContactForm(email, name, message);
-
-        res.status(200).end();
-        // or you can pass data to indicate success.
-        res.status(200).send({ isEmailSend: true });
-    })
-
-
-})
+		res.status(200).end();
+		// or you can pass data to indicate success.
+		res.status(200).send({ isEmailSend: true });
+	});
+});
 
 async function sendContactForm(email, name, message) {
-    try {
-        const r = await transport.sendMail({
-            from: `A message from ${name} `,
-            to: email,
-            subject: message,
-            html: `
+	try {
+		const r = await transport.sendMail({
+			from: `A message from ${name} `,
+			to: email,
+			subject: message,
+			html: `
             <h1>hola ${name}, Gracias!</h1>
             <hr/>
             <p>${message}</p>
-        `
-        });
-        return r;
-    } catch (e) {
-        return e;
-    }
+        `,
+		});
+		return r;
+	} catch (e) {
+		return e;
+	}
 }
-
